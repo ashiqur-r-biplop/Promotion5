dc6576da530d6496a66063f879261eabf59b49f7
ashiqur-r-biplop <biplops22@gmail.com>
2024-07-11 21:32:26 +0600

    added some code

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // toggle image
+const showImageContainer = document.getElementById("show-image");
+
+const toggleImage = (src, button) => {
+  showImageContainer.src = src;
+  const buttons = document.querySelectorAll(".image-button");
+  buttons.forEach((btn) => btn.classList.remove("active"));
+  // Add active class to the clicked button
+  button.classList.add("active");
+};
