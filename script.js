f99756611fa88e9d390ae5564e2a6412e18222a0
ashiqur-r-biplop <biplops22@gmail.com>
2024-07-12 17:07:47 +0600

    added some code

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ const toggleImage = (src, button) => {
   // Add active class to the clicked button
   button.classList.add("active");
 };
+
+// Recent Projects js
+// Get all gallery items and previews
+const items = document.querySelectorAll(".gallery__item-wrapper");
+const previews = document.querySelectorAll(".gallery__preview-wrapper");
+
+// Function to update the preview area
+function updatePreview(selectedItem) {
+  previews.forEach((preview) => {
+    preview.style.display =
+      preview.dataset.galleryItem === selectedItem ? "block" : "none";
+  });
+}
+
+// Show the first item by default
+updatePreview("1");
+
+// Add click event listeners to each item
+items.forEach((item) => {
+  item.addEventListener("click", function () {
+    const selectedItem = this.dataset.itemsItem;
+    updatePreview(selectedItem);
+  });
+});
